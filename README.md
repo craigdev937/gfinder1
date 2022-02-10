9de0effb49650c20920d1befa610a76fd6fb6d32
Craig <indigomx9@gmail.com>
2022-02-10 14:14:50 -0500

    Initial setup of the project.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Github Finder
+
+Initial setup of the project.   Using React, React Router 6, and Context API.
+
+
+
+
---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet corporis perferendis provident libero? Molestiae autem magni officiis consequuntur sunt aspernatur, dolorem at nam aliquam quod. Maxime autem porro esse ullam?</p>
+        </React.Fragment>
+    );
+};
+
+
+
